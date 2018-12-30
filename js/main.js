003346bcc298574056815747e9ab8d4c2e0ee34f
alecesana <cesaless@gmail.com>
2018-12-30 15:29:29 +1100

    smooth navbar, changed layout

--- js/main.js
+++ js/main.js
@@ -5,55 +5,44 @@
      $('.main-nav').toggleClass('active')
 }
 
-
-
-
-
 function removeClickFunction(){
- // $('.main-nav a').off()
-  console.log('functionremoved') 
+  //$('.main-nav a').off()
+  //console.log('functionremoved') 
   if ($('.main-nav').hasClass('active')){
    $('.main-nav').toggleClass('active');
   console.log('classremoved')
   }
 }
 
-
-    var mqList = window.matchMedia( "(min-width: 770px)" );  
-    mqList.addListener(mediaChange);
-    
- 
-  //mediaChange(mqList);
+  var mqList = window.matchMedia( "(min-width: 770px)" );  
+  mqList.addListener(mediaChange);
 
   function mediaChange(mqList){
   if (mqList.matches) {
-   /// console.log("bigwindow");
-   // console.log(mqList.matches);
     removeClickFunction();
-    smoothScroll();
+    
     } else {
-    // window width is less than 700px
     console.log("small window");
     $( '#navbar-icon').off().click(navIconActivate)
-   $( ".main-nav a" ).off().click(navIconActivate) 
-   smoothScroll() 
+    $( ".main-nav a" ).off().click(navIconActivate) 
+   
     }
+    smoothScroll() 
     
   }
 
-     // Add smooth scrolling to all links
-
-     function smoothScroll(){
-     $(".scroll").on('click', function(event) {
-       event.preventDefault();
-        var hash = this.hash;     
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top -50}, 500)        
-    });
+  function smoothScroll(){
+  $(".scroll").off().on('click', function(event) {
+    event.preventDefault();
+    var hash = this.hash;     
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top -50}, 500)        
+});
   }
 
-
-  $(document).ready(function(){    
-    mediaChange(mqList);
+  
+  $(document).ready(function(){  
+    mediaChange(mqList);  
+    
    })
 
