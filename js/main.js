245874a44b13657427d7784cb06bc578240c48a5
alecesana <cesaless@gmail.com>
2019-01-01 23:41:08 +1100

    adjusted main css and js, fixed first click on main-nav a shift bug

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 
 //var windowsize =  $(window).width();
 
- function navIconActivate(){ 
-     $('.main-nav').toggleClass('active')
+function navIconActivate(){ 
+  $('.main-nav').toggleClass('active')
 }
 
 function removeClickFunction(){
-  //$('.main-nav a').off()
-  //console.log('functionremoved') 
-  if ($('.main-nav').hasClass('active')){
-   $('.main-nav').toggleClass('active');
-  console.log('classremoved')
-  }
+$('.main-nav a').off()
+//console.log('functionremoved') 
+if ($('.main-nav').hasClass('active')){
+$('.main-nav').toggleClass('active');
+console.log('classremoved')
+}
 }
 
-  var mqList = window.matchMedia( "(min-width: 770px)" );  
-  mqList.addListener(mediaChange);
+var mqList = window.matchMedia( "(min-width: 770px)" );  
+mqList.addListener(mediaChange);
 
-  function mediaChange(mqList){
-  if (mqList.matches) {
-    removeClickFunction();
-    
-    } else {
-    console.log("small window");
-    $( '#navbar-icon').off().click(navIconActivate)
-    $( ".main-nav a" ).off().click(navIconActivate) 
-   
-    }
-    smoothScroll() 
-    
-  }
+function mediaChange(mqList){
+if (mqList.matches) {
+ removeClickFunction();
+ 
+ } else {
+ console.log("small window");
+ $( '#navbar-icon').off().click(navIconActivate)
+ $( ".main-nav a" ).off().click(navIconActivate) 
 
-  function smoothScroll(){
-  $(".scroll").off().on('click', function(event) {
-    event.preventDefault();
-    var hash = this.hash;     
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top -50}, 500)        
+ }
+ smoothScroll() 
+ 
+}
+
+function smoothScroll(){
+$(".scroll").on('click', function(event) {
+ event.preventDefault();
+ var hash = this.hash;     
+ $('html, body').animate({
+   scrollTop: $(hash).offset().top -50}, 500)        
 });
-  }
+}
+
 
   
-  $(document).ready(function(){  
-    mediaChange(mqList);  
-    
-   })
+ mediaChange(mqList);  
+ 
+
 
